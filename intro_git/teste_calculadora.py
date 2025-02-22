23af182586245b1a36249434ae3b505b1ac2bb39
Valdecir Nakao <valdecir.nakao@fatec.sp.gov.br>
2025-02-21 22:19:43 -0300

    test: testa a divisao

--- intro_git/teste_calculadora.py
+++ intro_git/teste_calculadora.py
@@ -3,3 +3,5 @@ a = 2
 b = 3
 print(f'{a} + {b} = {calculadora.somar(a, b)}')
 print(f'{a} - {b} = {calculadora.subtrair(a, b)}')
+print(f'{a} * {b} = {calculadora.multiplicar(a, b)}')
+print(f'{a} / {b} = {calculadora.dividir(a, b)}')
