44d24f7432054c87381bae27456ff981c36fa16c
Valdecir Hideki Nakao <valdecir.nakao@fatec.sp.gov.br>
2025-04-22 00:48:18 -0300

    learn(ap04-react): encerra o codigo 2.5.4
    - fizemos a responsividade para quatro caixinhas

---
+++ componentes-props/src/App.jsx
@@ -0,0 +1,22 @@
+const App = () => {
+    return (
+        <div className="container border rounded mt-2">
+            <div className="row">
+                <div className="col-sm-12 col-md-6 col-xxl-3">
+                    <p>Caixa 1</p>
+                </div>
+                <div className="col-sm-12 col-md-6 col-xxl-3">
+                    <p>Caixa 2</p>
+                </div>
+                <div className="col-sm-12 col-md-6 col-xxl-3">
+                    <p>Caixa 3</p>
+                </div>
+                <div className="col-sm-12 col-md-6 col-xxl-3">
+                    <p>Caixa 4</p>
+                </div>
+            </div>
+        </div>
+    )  
+}
+
+export default App
