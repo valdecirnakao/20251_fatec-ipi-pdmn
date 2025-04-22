380832ff88d7c1b3d2a591988eb8184f7083b760
Valdecir Hideki Nakao <valdecir.nakao@fatec.sp.gov.br>
2025-04-22 13:39:07 -0300

    learn(ap04-react): encerra a secao 2.5
    -vamos comecar a refatoracao criando um componente que representa pedidos

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -3,16 +3,55 @@ const App = () => {
         <div className="container border rounded mt-2">
             <div className="row">
                 <div className="col-sm-12 col-md-6 col-xxl-3">
-                    <p>Caixa 1</p>
+                    <Funcao />
                 </div>
                 <div className="col-sm-12 col-md-6 col-xxl-3">
-                    <p>Caixa 2</p>
+                    <div className="card">
+                        <div className="card-header text-muted">
+                            22/02/2022
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className="d-flex align-items-center">
+                                <i className="fa-solid fa-gamepad fa-2x fa-shake"></i>
+                            </div>
+                            <div className="border flex-grow-1 ms-3 py-2">
+                                <h4 className="text-center">Gamepad</h4>
+                                <p className="text-center">Controle Nintendo</p>
+                            </div>
+                        </div>    
+                    </div>
                 </div>
                 <div className="col-sm-12 col-md-6 col-xxl-3">
-                    <p>Caixa 3</p>
+                    <div className="card">
+                        <div className="card-header text-muted">
+                            22/04/2021
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className="d-flex align-items-center">
+                                <i className="fa-solid fa-hippo fa-2x fa-shake"></i>
+                            </div>
+                            <div className="border flex-grow-1 ms-3 py-2">
+                                <h4 className="text-center">Hipopótamo</h4>
+                                <p className="text-center">Hipopótamo Fêmea</p>
+                            </div>
+                        </div>    
+                    </div>
                 </div>
                 <div className="col-sm-12 col-md-6 col-xxl-3">
-                    <p>Caixa 4</p>
+                    <div className="card">
+                        <div className="card-header text-muted">
+                            22/04/2021
+                        </div>
+                        <div className="card-body d-flex">
+                            <div className="d-flex align-items-center">
+                                <i className="fa-solid fa-snowman fa-2x fa-shake"></i>
+                            </div>
+                            <div className="border flex-grow-1 ms-3 py-2">
+                                <h4 className="text-center">Boneco de neve</h4>
+                                <p className="text-center">Esse boneco de neve fica balançando sozinho</p>
+                            </div>
+                        </div>    
+                    </div>
                 </div>
             </div>
         </div>
