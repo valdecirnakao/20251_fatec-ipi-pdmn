b6fb791d67dae43d7f084fe92af0fd3ae8e372d2
Valdecir Hideki Nakao <valdecir.nakao@fatec.sp.gov.br>
2025-04-21 23:27:44 -0300

    learn(ap03-react): encerra a apostila

---
+++ primeiro-app/src/App.jsx
@@ -0,0 +1,43 @@
+const App = () => {
+  const estiloBotao = {
+    marginTop: 12,
+    paddingTop: 8,
+    paddingBottom: 8,
+    backgroundColor: 'blueviolet',
+    color: 'white',
+    border: 'none',
+    width: '100%',
+    borderRadius: 8}
+  const textoDoRotulo = 'Nome:'
+  const obterTextoDoBotao = () => {return 'Enviar'}
+  const aoClicar = alert('Clicou')   
+  return (
+    <div 
+      style={{
+        width: 768
+        ,margin: 'auto'
+        ,backgroundColor: '#EEE'
+        ,padding: 12
+        ,borderRadius: 8}}>
+          <label className='rotulo' htmlFor='nome' style={{display: 'block', marginBottom: 4}}>
+          {textoDoRotulo}
+          </label>
+          <input type="text" id="nome" style={{
+            paddingTop: 8,
+            paddingBottom: 8,
+            borderStyle: 'hidden',
+            width: '100%',
+            borderRadius: 8,
+            outline: 'none'}}/>
+
+            
+          <button 
+          onClick={aoClicar}
+          style={estiloBotao}>
+          {obterTextoDoBotao()}
+          </button>
+    </div>
+  )
+}
+
+export default App;
