5d1d76e0648c6c38ddd92196936ded6319b2fc01
Valdecir Hideki Nakao <valdecir.nakao@fatec.sp.gov.br>
2025-04-23 21:33:13 -0300

    learn(ap07-react): encerra a secao 2.2

---
+++ exibe-fotos/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Exibir uma lista de...</h1>
+      <i className="pi pi-search"></i>
+      <i className="pi pi-linkedin"></i>
+    </div>
+  )
+}
+
+export default App
